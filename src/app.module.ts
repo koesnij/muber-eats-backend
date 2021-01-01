6e3bf92987dff491b4aede7ca352a61255d4ac87
문진석 <m_jinseok@naver.com>
2021-01-01 21:20:00 +0900

    2.3 TypeORM Setup
    - install TypeORM packages
    - connect to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
     RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'koesnij',
+      password: '-', // local에서 접속할 때는 비밀번호를 묻지 않음
+      database: 'muber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true, // on memory
     }),
