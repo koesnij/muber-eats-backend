5c549d90d6bbd638401396e2eeab3c01c577208b
문진석 <m_jinseok@naver.com>
2021-01-01 21:59:50 +0900

    2.5 Configuring ConfigService
    - write db config (.env.test)
    - fix ENV -> NODE_ENV (package.json)

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,14 +9,15 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
     ConfigModule.forRoot({
       isGlobal: true, // 앱 어디에서든지 접근 가능
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'koesnij',
-      password: '-', // local에서 접속할 때는 비밀번호를 묻지 않음
-      database: 'muber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // string -> number
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
