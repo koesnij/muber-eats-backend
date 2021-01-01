02bbfa9dc4e5855fd786cf767b25238d2625ed2c
문진석 <m_jinseok@naver.com>
2021-01-01 14:54:20 +0900

    1.4 InputTypes and ArgumentTypes
    - Mutation
    - InputType
    - ArgumentType

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -5,6 +5,12 @@ export class Restaurant {
   @Field(type => String /*return type function */) // 가독성
   name: string;
 
-  @Field(type => Boolean, { nullable: true })
-  isGood?: boolean;
+  @Field(type => Boolean)
+  isVegan: boolean;
+
+  @Field(type => String)
+  address: string;
+
+  @Field(type => String)
+  ownerName: string;
 }
--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,4 +1,5 @@
-import { Args, Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
@@ -7,5 +8,10 @@ export class RestaurantResolver {
   restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
     console.log(veganOnly);
     return [];
+  }
+
+  @Mutation(returns => Boolean)
+  createRestaurant(@Args() CreateRestaurantDto: CreateRestaurantDto): boolean {
+    return true;
   }
 }
