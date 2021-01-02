01a396427014575bad1a75349d0f5da4017610a6
문진석 <m_jinseok@naver.com>
2021-01-02 19:24:04 +0900

    3.0 Our First Entity
    - ObjectType과 Entity 같이 사용하기
    - DB Entity와 TypeORM 연결하기
    - TypeORM의 synchronize 옵션

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,16 +1,30 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
+  @PrimaryGeneratedColumn()
+  @Field(type => Number)
+  id: number;
+
   @Field(type => String /*return type function */) // 가독성
+  @Column()
   name: string;
 
   @Field(type => Boolean)
+  @Column()
   isVegan: boolean;
 
   @Field(type => String)
+  @Column()
   address: string;
 
   @Field(type => String)
-  ownerName: string;
+  @Column()
+  ownersName: string;
+
+  @Field(type => String)
+  @Column()
+  categoryName: string;
 }
