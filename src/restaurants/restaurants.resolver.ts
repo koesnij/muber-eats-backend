0cbf81162b39e2b7dab98b5289bb580fa94a15e5
문진석 <m_jinseok@naver.com>
2021-01-02 19:59:11 +0900

    3.2 Injecting The Repository
    - DB에 접근하기
    - RestaurantModule, RestaurantResolver, RestaurantService, RestaurantRepository

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,13 +1,15 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
+
   @Query(returns => [Restaurant] /* graphql type */)
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    console.log(veganOnly);
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
 
   @Mutation(returns => Boolean)
