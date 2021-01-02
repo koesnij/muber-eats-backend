ccbd57e9733e9d3e33908bbe5eb66a17c81344ab
문진석 <m_jinseok@naver.com>
2021-01-02 21:19:16 +0900

    3.5 Mapped Types
    - #3.4의 DTO 누락 에러 해결
      - Mapped Types 사용
    	- InputType으로의 변환
    	- Validating

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,6 +1,9 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { IsBoolean, IsString } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+// @InputType() 스키마에 포함됨 -- Restaurant 이름으로 두개가 됨
+@InputType({ isAbstract: true }) //스키마 등록 X, 그냥 extend
 @ObjectType()
 @Entity()
 export class Restaurant {
@@ -10,18 +13,22 @@ export class Restaurant {
 
   @Field(type => String /*return type function */) // 가독성
   @Column()
+  @IsString()
   name: string;
 
   @Field(type => Boolean)
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(type => String)
   @Column()
+  @IsString()
   address: string;
 
   @Field(type => String)
   @Column()
+  @IsString()
   ownersName: string;
 
   @Field(type => String)
