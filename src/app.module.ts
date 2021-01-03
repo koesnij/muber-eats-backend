e3922a1011b5f7317ba1eac9ba94ac7dd0917a97
문진석 <m_jinseok@naver.com>
2021-01-03 11:46:22 +0900

    4.2 user Resolver and Service
    - Inject User Repository
    - UsersService, UsersResolver 작성
    - UsersModule에 UsersService, UsersResolver 연결

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import * as Joi from 'joi'; // JS 패키지를 TS에서 import하는 방법
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { RestaurantsModule } from './restaurants/restaurants.module';
-import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
 import { User } from './users/entities/user.entity';
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,12 @@
+import { Resolver, Query } from '@nestjs/graphql';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver(of => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {} // inject UsersService
+  @Query(returns => Boolean)
+  hi() {
+    return true;
+  }
+}
