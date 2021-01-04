8dfe7afb0b5cd09bf5774b09749a67e8e9a338ec
문진석 <m_jinseok@naver.com>
2021-01-04 10:58:51 +0900

    4.9 Log In part II
    - 로그인 로직 완성하기
      - User Entity - checkPassword() 작성
      - Users Service - login() 작성
      - Users Resolver - login() 작성
    	- LoginOutput - token? 수정

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -19,13 +19,7 @@ export class UsersResolver {
     @Args('input') createAccountInput: CreateAccountInput,
   ): Promise<CreateAccountOutput> {
     try {
-      const { ok, error } = await this.usersService.createAccount(
-        createAccountInput,
-      );
-      return {
-        ok,
-        error,
-      };
+      return this.usersService.createAccount(createAccountInput);
     } catch (error) {
       // unexpected error
       return {
@@ -37,6 +31,10 @@ export class UsersResolver {
 
   @Mutation(returns => LoginOutput)
   async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
-    return;
+    try {
+      return this.usersService.login(loginInput);
+    } catch (error) {
+      return { ok: false, error };
+    }
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dtos/create-account.dto';
+import { LoginInput } from './dtos/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -29,6 +30,32 @@ export class UsersService {
     } catch (error) {
       // make error
       return { ok: false, error: "Couldn't create account" };
+    }
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    try {
+      // todo : JWT Token
+      const user = await this.users.findOne({ email });
+      if (!user) {
+        return { ok: false, error: '해당 이메일이 존재하지 않습니다.' };
+      }
+      const passwordCorrect = await user.checkPassword(password);
+      if (!passwordCorrect) {
+        return {
+          ok: false,
+          error: '비밀번호가 일치하지 않습니다.',
+        };
+      }
+      return {
+        ok: true,
+        token: 'test token',
+      };
+    } catch (error) {
+      return { ok: false, error };
     }
   }
 }
