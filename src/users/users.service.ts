578c901255004a7aa017d6e44342e426abde492a
문진석 <m_jinseok@naver.com>
2021-01-08 11:59:13 +0900

    6.5 Mailgun Setup
    - Verification 인증 후 삭제 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -137,6 +137,7 @@ export class UsersService {
       if (verification) {
         verification.user.verified = true;
         await this.users.save(verification.user);
+        await this.verifications.delete(verification.id);
         return {
           ok: true,
         };
