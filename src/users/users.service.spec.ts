f14e826b2485419a306e23d1b5c0c6c07ea25626
문진석 <m_jinseok@naver.com>
2021-01-09 17:21:51 +0900

    7.2 Mocking II
    - Unit Testing 목적 설명
    - Type MockRepository 정의하기
      - Partial, Record (typescript)

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -6,6 +6,7 @@ import { MailService } from 'src/mail/mail.service';
 import { JwtService } from 'src/jwt/jwt.service';
 import { User } from './entities/user.entity';
 import { Verification } from './entities/verification.entity';
+import { Repository } from 'typeorm';
 
 const mockRepository = {
   findOne: jest.fn(), // creates a mock function
@@ -22,8 +23,13 @@ const mockMailService = {
   sendVerificationEmail: jest.fn(),
 };
 
+type MockRepository<T = any> = Partial<
+  Record<keyof Repository<User>, jest.Mock>
+>;
+
 describe('UsersService', () => {
   let service: UsersService;
+  let usersRepository: MockRepository;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
@@ -48,10 +54,17 @@ describe('UsersService', () => {
       ],
     }).compile();
     service = module.get<UsersService>(UsersService);
+    usersRepository = module.get(getRepositoryToken(User));
   });
 
   it('be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createAccount', () => {
+    it('should fail if user exists', () => {
+      /** 존재하는 유저로 속여야 함(가짜로) 모든걸 가짜로 */
+    });
   });
 
   // 할 일
