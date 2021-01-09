75c0bdfe2e62461be0039088c00fc7ef334942fd
문진석 <m_jinseok@naver.com>
2021-01-09 16:27:23 +0900

    7.1 Mocking
    - 모의 객체(mock) 사용하여 Dependencies 에러 해결
      - MockRepository
    	- MockMailService
    	- MockJwtService

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,51 @@
 import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
 import { UsersService } from './users.service';
+import { MailService } from 'src/mail/mail.service';
+import { JwtService } from 'src/jwt/jwt.service';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
+
+const mockRepository = {
+  findOne: jest.fn(), // creates a mock function
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockJwtService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+
+const mockMailService = {
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User), // User Entity의 Repository Token
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: JwtService,
+          useValue: mockJwtService,
+        },
+        {
+          provide: MailService,
+          useValue: mockMailService,
+        },
+      ],
     }).compile();
     service = module.get<UsersService>(UsersService);
   });
