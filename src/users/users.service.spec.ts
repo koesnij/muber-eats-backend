bfb0e9ea13eb85082acc42b2f3ea8b474ef06d5c
문진석 <m_jinseok@naver.com>
2021-01-11 18:34:54 +0900

    7.10 editProfile Test part I
    - editProfile() - should change email

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -212,6 +212,34 @@ describe('UsersService', () => {
     });
   });
 
+  describe('editProfile', () => {
+    it('should change email', async () => {
+      const oldUser = { email: 'a@old.com', verified: true };
+      const editProfileArgs = { userId: 1, input: { email: 'b@new.com' } };
+      const newVerification = { code: 'code' };
+      const newUser = { verified: false, email: editProfileArgs.input.email };
+
+      usersRepository.findOne.mockResolvedValue(oldUser);
+      verificationRepository.create.mockReturnValue(newVerification); // create doesnt return a promise
+      verificationRepository.save.mockResolvedValue(newVerification); // create doesnt return a promise
+
+      await service.editProfile(editProfileArgs.userId, editProfileArgs.input);
+      expect(usersRepository.findOne).toHaveBeenCalledTimes(1);
+      expect(usersRepository.findOne).toHaveBeenCalledWith(
+        editProfileArgs.userId,
+      );
+
+      expect(verificationRepository.create).toHaveBeenCalledWith({
+        user: newUser,
+      });
+      expect(verificationRepository.save).toHaveBeenCalledWith(newVerification);
+
+      expect(mailService.sendVerificationEmail).toHaveBeenCalledWith(
+        newUser.email,
+        newVerification.code,
+      );
+    });
+  });
   // 할 일
   it.todo('editProfile');
   it.todo('verifyEmail');
