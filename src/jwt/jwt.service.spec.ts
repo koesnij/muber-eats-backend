eb2527507a9d13d077551d8346e7571673acba6f
문진석 <m_jinseok@naver.com>
2021-01-12 14:35:42 +0900

    8.1 JWT Sign Test
    - sign() - should return a signed token
      - mocking jsonwebtoken
      - expect jsonwebtoken

--- src/jwt/jwt.service.spec.ts
+++ src/jwt/jwt.service.spec.ts
@@ -1,8 +1,14 @@
 import { Test } from '@nestjs/testing';
+import * as jwt from 'jsonwebtoken';
 import { CONFIG_OPTIONS } from 'src/common/common.constants';
 import { JwtService } from './jwt.service';
 
+const TOKEN = 'TOKEN';
 const TEST_KEY = 'testKey';
+
+jest.mock('jsonwebtoken', () => ({
+  sign: jest.fn(() => TOKEN),
+}));
 
 describe('JwtService', () => {
   let service: JwtService;
@@ -21,6 +27,16 @@ describe('JwtService', () => {
     expect(service).toBeDefined();
   });
 
-  it.todo('sign');
+  describe('sign', () => {
+    it('should return a signed token', () => {
+      const ID = 1;
+
+      const token = service.sign(ID);
+      expect(jwt.sign).toHaveBeenCalledTimes(1);
+      expect(jwt.sign).toHaveBeenCalledWith({ id: ID }, TEST_KEY);
+      expect(token).toEqual(TOKEN);
+    });
+  });
+
   it.todo('verify');
 });
