9ad9da3779040a3c090b36929315f4cfd18c903e
문진석 <m_jinseok@naver.com>
2021-01-15 19:50:53 +0900

    9.3 Testing createAccount part II
    - '--detectOpenHandles' : open handle 에러 해결
    - createAccount() - should fail if account already exists

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -5,6 +5,12 @@ import * as request from 'supertest';
 
 import { AppModule } from '../src/app.module';
 
+jest.mock('got', () => {
+  return {
+    post: jest.fn(),
+  };
+});
+
 const GRAPHQL_ENDPOINT = '/graphql';
 
 describe('UsersModule (e2e)', () => {
@@ -49,7 +55,29 @@ describe('UsersModule (e2e)', () => {
           expect(res.body.data.createAccount.error).toBe(null);
         });
     });
-    it.todo('should fail if account already exists');
+    it('should fail if account already exists', () => {
+      return request(app.getHttpServer())
+        .post(GRAPHQL_ENDPOINT)
+        .send({
+          query: `
+            mutation {
+              createAccount(input: {
+                email: "${EMAIL}",
+                password: "12345",
+                role: Owner
+              }) {
+                ok
+                error
+              }
+            }
+          `,
+        })
+        .expect(200)
+        .expect(res => {
+          expect(res.body.data.createAccount.ok).toBe(false);
+          expect(res.body.data.createAccount.error).toEqual(expect.any(String));
+        });
+    });
   });
 
   it.todo('userProfile');
