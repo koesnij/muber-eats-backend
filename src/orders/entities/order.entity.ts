7a621f2e951348a07a3798e32896e1b98c017cb2
문진석 <m_jinseok@naver.com>
2021-01-29 10:20:33 +0900

    11.5 Create Order part I
    - Orders Resolver 생성
    - Orders Service 생성
    - CreateOrder DTO 생성

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -44,8 +44,8 @@ export class Order extends CoreEntity {
   @JoinTable()
   dishes: Dish[];
 
-  @Column()
-  @Field(type => Float)
+  @Column({ nullable: true })
+  @Field(type => Float, { nullable: true })
   total: number;
 
   @Column({ type: 'enum', enum: OrderStatus })
--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrdersResolver } from './orders.resolver';
+import { OrdersService } from './orders.service';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Order])],
+  providers: [OrdersService, OrdersResolver],
+})
 export class OrdersModule {}
